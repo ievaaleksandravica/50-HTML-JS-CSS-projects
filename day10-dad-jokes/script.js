afb8d8db4ba38c1f4f2a6be318d8ae23940eff84
Ieva Aleksandravica <ieva.aleksandravica@gmail.com>
2021-12-27 17:43:55 +0100

    fetch data from api

---
+++ day10-dad-jokes/script.js
@@ -0,0 +1,22 @@
+const jokeEl = document.getElementById('joke')
+const jokeBtn = document.getElementById('jokeBtn')
+
+// const generateJoke = () => {
+//     fetch("https://icanhazdadjoke.com/", config)
+//         .then((response) => response.json())
+//         .then((data) => {
+//             jokeEl.innerHTML = data.joke
+//         })
+// }
+
+const generateJoke = async () => {
+    const config = {                headers: { "Accept": "application/json" } }
+
+    const response = await fetch("https://icanhazdadjoke.com/", config)
+    const data = await response.json()
+    jokeEl.innerHTML = data.joke
+}
+
+jokeBtn.addEventListener('click', generateJoke)
+
+generateJoke()
