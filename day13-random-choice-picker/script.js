8a26d942ffad385267a10fe70e3fb2940d948210
Ieva Aleksandravica <ieva.aleksandravica@gmail.com>
2021-12-29 17:44:23 +0100

    pick random tag

--- day13-random-choice-picker/script.js
+++ day13-random-choice-picker/script.js
@@ -12,8 +12,48 @@ const createTags = (input) => {
     })
 }
 
+const pickRandomTag = () => {
+    const tags = document.querySelectorAll('.tag')
+    return tags[Math.floor(Math.random() * tags.length)]
+}
+
+const highlightTag = (tag) => {
+    tag.classList.add('highlight')
+}
+
+const unhighlightTag = (tag) => {
+    tag.classList.remove('highlight')
+}
+
+const randomSelect = () => {
+    const times = 30
+
+    const interval = setInterval(() => {
+        const randomTag = pickRandomTag()
+        highlightTag(randomTag)
+        setTimeout(() => {
+            unhighlightTag(randomTag)
+        }, 100);
+    }, 100);
+
+    setTimeout(() => {
+        clearInterval(interval)
+        setTimeout(() => {
+            const randomTag = pickRandomTag()
+            highlightTag(randomTag)
+        }, 100);
+    }, times * 100);
+}
+
 textareaEl.focus()
 
-textareaEl.addEventListener("keydown",(event) => {
+textareaEl.addEventListener("keyup",(event) => {
     createTags(event.target.value)
+
+    if(event.key === "Enter") {
+        setTimeout(() => {
+            event.target.value = ""
+        }, 10)
+        randomSelect()
+    }
 })
