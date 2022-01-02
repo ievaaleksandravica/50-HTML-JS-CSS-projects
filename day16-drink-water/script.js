ba5450fe31217c9bc1b9a9734bb9a498bbee8afc
Ieva Aleksandravica <ieva.aleksandravica@gmail.com>
2022-01-02 11:56:40 +0100

    big cup - liters

--- day16-drink-water/script.js
+++ day16-drink-water/script.js
@@ -3,6 +3,28 @@ const liters = document.getElementById('liters')
 const percentage = document.getElementById('percentage')
 const remained = document.getElementById('remained')
 
+const updateBigCup = () => {
+    const fullCups = document.querySelectorAll('.cup-small.full').length
+    const totalCups = smallCups.length
+
+    if (fullCups === 0) {
+        percentage.style.visibility = "hidden"
+        percentage.style.height = 0
+    } else {
+        percentage.style.visibility = 'visible'
+        percentage.style.height = `${fullCups/totalCups * 330}px`
+        percentage.innerText = `${fullCups/totalCups * 100}%`
+    }
+
+    if (fullCups === totalCups) {
+        remained.style.visibility = "hidden"
+        remained.style.height = 0
+    } else {
+        remained.style.visibility = "visible"
+        liters.innerText = `${2 - (250 * fullCups / 1000)}L`
+    }
+}
+
 const highlightCups = (index) => {
     if(smallCups[index].classList.contains("full") && !smallCups[index].nextElementSibling.classList.contains("full")) {
         index--
@@ -16,10 +38,14 @@ const highlightCups = (index) => {
             cup.classList.remove('full')
         }
     })
+    updateBigCup()
 }
+
+
+updateBigCup()
 
 smallCups.forEach((cup, index) => {
    cup.addEventListener('click', () => {
-       highlightCups(index)
+       highlightCups(index)   
    })
 })
