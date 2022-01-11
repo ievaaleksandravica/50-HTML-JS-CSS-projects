78d4fdb96578fe36aff8eddef3d760daeed8eb22
Ieva Aleksandravica <ieva.aleksandravica@gmail.com>
2022-01-11 15:30:24 +0100

    change buttons

--- day22-drawing-app/script.js
+++ day22-drawing-app/script.js
@@ -1,7 +1,14 @@
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
+ 
+const increaseBtn = document.getElementById("increase");
+const decreaseBtn = document.getElementById("decrease");
+const sizeEl = document.getElementById("size");
+const colorEl = document.getElementById("color");
+const clearEl = document.getElementById("clear");
 
-let size = 20
+
+let size = 10
 let isPressed = false
 let color = "black"
 let x 
@@ -49,3 +56,32 @@ function drawLine(x1, y1, x2, y2) {
     ctx.stroke()
 }
 
+function updateSizeOnScreen() {
+    sizeEl.innerText = size
+}
+
+colorEl.addEventListener("change", (e) => {
+    color = e.target.value
+})
+
+increaseBtn.addEventListener("click", () => {
+    size += 5
+    if(size > 50) {
+        size = 50
+    }
+
+    updateSizeOnScreen()
+})
+
+decreaseBtn.addEventListener("click", () => {
+    size -= 5
+    if(size < 5) {
+        size = 5
+    }
+
+    updateSizeOnScreen()
+})
+
+clearEl.addEventListener("click", () => {
+    ctx.clearRect(0,0, canvas.width, canvas.height)
+})
